785afc0db5f6aed0ab41a0b9fe54d4f7f8b84aae
miche2lec <32557872+miche2lec@users.noreply.github.com>
2019-12-15 21:37:31 +0100

    help sample

--- app/src/main/java/com/ait/mealkitdeliveryapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/ait/mealkitdeliveryapp/ui/notifications/NotificationsFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ait.mealkitdeliveryapp.R
+import kotlinx.android.synthetic.main.fragment_notifications.view.*
+import org.w3c.dom.Text
 
 class NotificationsFragment : Fragment() {
 
@@ -22,10 +25,16 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        var animation = AnimationUtils.loadAnimation(activity, R.anim.dropdown)
+        root.tvTestQ.setOnClickListener{
+            if (root.tvTestA.visibility == View.GONE){
+                root.tvTestA.visibility = View.VISIBLE
+                root.tvTestA.animation = animation
+            } else {
+                root.tvTestA.visibility = View.GONE
+            }
+
+        }
         return root
     }
 }
